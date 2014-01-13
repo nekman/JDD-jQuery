fc62209df715b1c74d74b094408cf69454693c7d
nekman <nekman@A01C00891.ad.sogeti.se>
2014-01-13 20:25:29 +0100

    Minor refactor + added comments

--- js/shoppingcart/pubsub.js
+++ js/shoppingcart/pubsub.js
@@ -1,33 +1,41 @@
 /**
- * Modifierat exempel från Patterns föreläsningen.
- * Gjort det möjligt att skicka in "context".
+ * Modifierat exempel från JDD-patterns föreläsningen.
+ *
+ * > Utökar jQuery med metoderna jQuery.subscribe / jQuery.publish.
+ * > Ändrat  "topics" till private scope.
+ * > Gör det möjligt att skicka in "context" som ett argument.
+ *   Kunde tidigare göras genom att anropa med "bind" eller med:
+ * 
+ *   $.proxy(this, $.subscribe('/example', callback));
  */
 (function($) {
+  'use strict';
 
+  // private
   var topics = {};
 
   $.publish = function(topicsName) {
-    var args = Array.prototype.slice.call(arguments, 1);
+    var args = Array.prototype.slice.call(arguments, 1),
+        subscribers = topics[topicsName];
 
-    if (!topics[topicsName]) {
+    if (!subscribers) {
       return;
     }
 
-    for (var i = 0; i < topics[topicsName].length; i++) {
-      var topic = topics[topicsName][i];
-      topic.callback.apply(topic.context, args);
-    }
-  },
+    $.each(subscribers, function() {
+      this.fn.apply(this.context, args);
+    });
+  };
 
-  $.subscribe = function(topicsName, context, fn) {
+  $.subscribe = function(topicsName, context, callback) {
     if (!topics[topicsName]) {
       topics[topicsName] = [];
     }
 
     topics[topicsName].push({
       context: context, 
-      callback: fn
+      fn: callback
     });
-  }
+  };
 
 })(jQuery);
